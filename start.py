ee6b0a0dd2a36a37667109384740428b7b17fdf5
Marius Ranta <marius.v.ranta@utu.fi>
2023-12-03 15:08:03 +0000

    Replace start.py

--- start.py
+++ start.py
@@ -1,6 +1,23 @@
 import random
+import pygame
 
-koko = input("Anna koko (iso tai pieni): ")
+def game_over(): 
+    leveys, korkeus = 1600, 900
+    ikkuna = pygame.display.set_mode((leveys, korkeus))
+    pygame.display.set_caption("Game Over")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        lataa_kuva = pygame.image.load("horror.jpg").convert()
+        ikkuna.blit(lataa_kuva, (0, 0))
+        pygame.display.flip()
+
+print("Pelaat peliä Miinaharava Extended Horror Experience!")
+print("Pelilaudan koot: pieni 5x5, iso 10x10")
+koko = input("Anna pelilaudan koko (pieni / iso): ")
 
 if koko == "iso":
     n = 10
@@ -10,12 +27,24 @@ elif koko == "pieni":
     y_n = 2
 
 
-matriisi = [['|_|' for _ in range(n)] for _ in range(n)]
+matriisi = [['/' for _ in range(n)] for _ in range(n)]
+
+
+rivi = int(input("Anna rivi: "))
+sarake = int(input("Anna sarake: "))
+
+ruutu = "0"
+matriisi[rivi][sarake] = ruutu
+
 
 for _ in range(y_n):
     satunnainen_rivi = random.randint(0, n - 1)
     satunnainen_sarake = random.randint(0, n - 1)
-    matriisi[satunnainen_rivi][satunnainen_sarake] = 'B'
+    if matriisi[satunnainen_rivi][satunnainen_sarake] == ruutu:
+        y_n += 1
+    else:
+        matriisi[satunnainen_rivi][satunnainen_sarake] = 'B'
 
 for rivi in matriisi:
     print(' '.join(rivi))
+
