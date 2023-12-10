726942cc12f0f72e1995270b624f3642d7128ab9
Marius Ranta <marius.v.ranta@utu.fi>
2023-12-10 02:11:10 +0000

    Replace start.py

--- start.py
+++ start.py
@@ -1,7 +1,13 @@
-import random
-import pygame
+"""Viimeisin toimiva versio."""
 
-def game_over(): 
+import pygame
+import random
+from os import system, name
+
+def game_over():
+    """Avaa koko näytön kokoisen ikkunan jossa on kuva."""
+    
+    # ei toimi atm. ei tietoa onko vika funktion sisä- vai ulkopuolella vai tavassa käyttää funktiota
     leveys, korkeus = 1600, 900
     ikkuna = pygame.display.set_mode((leveys, korkeus))
     pygame.display.set_caption("Game Over")
@@ -13,38 +19,218 @@ def game_over():
 
         lataa_kuva = pygame.image.load("scoreboard.jpg").convert()
         ikkuna.blit(lataa_kuva, (0, 0))
-        pygame.display.flip()
+        pygame.display.flip()   
 
-print("Pelaat peliä Miinaharava Extended Horror Experience!")
-print("Pelilaudan koot: pieni 5x5, iso 10x10")
-koko = input("Anna pelilaudan koko (pieni / iso): ")
+def kysy_koko():
+    """Pelin esittely ja pelikentän koon kysyntä.
 
-if koko == "iso":
-    n = 10
-    y_n = 5
-elif koko == "pieni":
-    n = 5
-    y_n = 2
+        Peli on koodattu siten, että pelikentän kokoa ja pommien lukumäärää voi halutessaan vaihtaa helposti
+        ja koko muu peli adaptoituu niiden mukaan.
+    """
+    
+    global n
+    global lkm
+    
+    print("_______")
+    print("|     |")
+    print("|  D: |")
+    print("|_____|")
+    print("Pelaat peliä Miinaharava Extended Horror Experience!")
+    print()
+    print("Pelilaudan koot: pieni 5x5, iso 10x10")
+    print()
 
+    while True:
+        koko = input("Anna pelilaudan koko (pieni / iso): ")
+        
+        # koko määrittää rivien/sarakkeiden määrän (n) sekä pommine lukumäärän (lkm)
+        if koko == "pieni":
+            n = 5
+            lkm = 2
+            break
+        elif koko == "iso":
+            n = 10
+            lkm = 5
+            break
+        else:
+            print(f"Opettele kirjottaa, pls!")
+            
+def aseta_pommit():
+    """Asettaa pommit satunnaisiin paikkoihin backend-matriisissa."""
+    
+    global mat_back
+    global x
+    global y
+    global lkm
+    
+    # lkm = pommien lukumäärä (määritetään kysy_koko()-funktiossa)
+    for _ in range(lkm):
+        sat_rivi = random.randint(0, n - 1)
+        sat_sarake = random.randint(0, n - 1)
+        mat_back[sat_rivi][sat_sarake] = -1
 
-matriisi = [['/' for _ in range(n)] for _ in range(n)]
+def tulosta_ruudukko() -> str:
+    """Tulostaa pelilaudan ruudukkona."""
+    
+    global n
+    global mat_front
+    
+    # tulostetaan taulukon katto
+    print("______" * n + "_")
+    
+    # tulostetaan taulukko vaakarivi kerrallaan
+    for lista in mat_front:
+        print("|     " * n + "|")
+        
+        # iteroidaan listan alkiot, jotta saadaa rivin jokaiseen slottiin oikeat arvot
+        rivi = ""
+        for alkio in range(0, n):
+            rivi += f"|  " + str(lista[alkio]) + "  "
+        
+        # lisätään vaakariville vielä "päätypala"
+        rivi += "|"
+        print(rivi)
+        print("|_____" * n + "|")
 
+def pelaajan_valinta():
+    """Kysytään avattavan ruudun koordinaatit."""
+    
+    global toiminto
+    global x
+    global y
+    
+    # looppi yksinkertaista virheenkäsittelyä varten
+    while True:
+        try:
+            # "" / "å" / "f"
+            toiminto = input("Toiminto: ")
+            # jos haluu pelistä vittuun
+            if toiminto == "å":
+                exit()
+                
+            x = int(input("Anna x: "))
+            y = int(input("Anna y: "))
+            
+            if (toiminto != "") and (toiminto != "f"):
+                print("Virheellinen toiminto!")
+            elif (x > n) or (x < 0) or (y > n) or (y < 0):
+                print(f"Anna lukuja vain väliltä 0 - {n}.")
+            else:
+                break
+        except ValueError:
+            print("Vain numerot kelpaavat!")
+    
+    # muutetaan koordinaatit python-ystävällisiksi (listojen eka on indeksi 0 eikä 1!)
+    x -= 1
+    y -= 1
+    
+def numerointi():
+    """Numeroidaan backend-matriisissa kosketuksissa olevien pommien määrä per ruutu."""
+    
+    global mat_back
+    
+    for i in range(len(mat_back)):
+        for j in range(len(mat_back[i])):
+            # jos huomataan -1 eli pommi:
+            if mat_back[i][j] == -1:
+                # rajataan alue, ja jonka kaikkien -1 poikkeavien arvojen tilalle lisätään yhtä isompi luku
+                # chatgpt setit, linkki jossain
+                for x in range(max(0, i-1), min(i+2, len(mat_back))):
+                    for y in range(max(0, j-1), min(j+2, len(mat_back[i]))):
+                        if mat_back[x][y] != -1:
+                            mat_back[x][y] += 1
 
-rivi = int(input("Anna rivi: "))
-sarake = int(input("Anna sarake: "))
-
-ruutu = "0"
-matriisi[rivi][sarake] = ruutu
-
-
-for _ in range(y_n):
-    satunnainen_rivi = random.randint(0, n - 1)
-    satunnainen_sarake = random.randint(0, n - 1)
-    if matriisi[satunnainen_rivi][satunnainen_sarake] == ruutu:
-        y_n += 1
+def ruutujen_muutos_frontissa():
+    """Muuttaa ruutuja pelaajan antamien koordinaattien ympäriltä."""
+    
+    global nakyvat
+    global x
+    global y
+    
+    # lisätään koordinaatit jo valittujen joukkoon (täl listal ei atm mitään käyttöö)
+    nakyvat.append((x, y))
+    
+    # liputus
+    if toiminto == "f":
+        if mat_front[y][x] != "F":
+            mat_front[y][x] = "F"
+        # lipun muutos tyhjäksi
+        elif mat_front[y][x] == "F":
+            mat_front[y][x] = " "
+            
+    # ruudun avaus moodi
     else:
-        matriisi[satunnainen_rivi][satunnainen_sarake] = 'B'
+        # jos ruutu on 0 mat_backissa...
+        if mat_back[y][x] == 0:
+            # ...niin muutetaan se fronttiin 0
+            mat_front[y][x] = mat_back[y][x]
+            
+            # tähän se koodi jolla se avaa vain ja ainoastaan ympäriltä ne ruudut jotka ei oo -1... tää ei toimi vv
+            for i in range(max(0, y-1), min(y+2, len(mat_back))):
+                for j in range(max(0, x-1), min(x+2, len(mat_back[i]))):
+                    if mat_back[y][x] != -1:
+                        mat_front[y][x] = mat_back[y][x]
+        
+        # jos osutaan pommiin ;)
+        elif mat_back[y][x] == -1:
+            mat_front[y][x] = "M"
+            g_over = True
+        else:
+            # muutetaan se fronttiin juuri siksi luvuksi (tähhh?)
+            mat_front[y][x] = mat_back[y][x]          
 
-for rivi in matriisi:
-    print(' '.join(rivi))
+def ohjeet() -> str:
+    """Tulostaa ohjeet / perusnäkymän pelin alettua."""
+    
+    print("Miinaharava Extended Horror Experience!")
+    print()
+    print("Toiminnot: ruudun avaus \"\" / liputus \"f\"")
 
+def tyhjenna_terminaali():
+    """Komento tyhjentää terminaalin käyttöjärjestelmän huomioiden."""
+    
+    # windows
+    if name == "nt":
+        _ = system("cls")
+    # mac / linux
+    # else:
+        # _ = system("clear")
+        
+      
+# kvg mitä meinaa :D (taitaa olla vähän turha mut näyttää ainaki kivalt)
+if __name__ == "__main__":
+    """Käytännössä peli pelataan tässä."""
+    
+    tyhjenna_terminaali()
+    
+    # kysytään alkuarvoja
+    kysy_koko()
+    
+    # backend-matriisi
+    mat_back = [[0 for rivi in range(n)] for sarake in range(n)] 
+    # frontend-matriisi
+    mat_front = [[" " for rivi in range(n)] for sarake in range(n)]
+    # listataan liputetut koordinaatit (tupleina?) atm hyödytön
+    liput = []
+    # listataan jo näkyvissä olevat ruudut (tupleina?) atm hyödytön
+    nakyvat = []
+    
+    # asetetaan pommit ja niiden ympärille numerot mat_backiin
+    aseta_pommit()
+    numerointi()
+    
+    # tällä hetkellä ruutujen_muutos_frontissa()-funktiossa testataan pommiin osumista (ei toimi)
+    g_over = False
+    
+    while not g_over:
+        tyhjenna_terminaali()
+        ohjeet()
+        tulosta_ruudukko()
+        pelaajan_valinta()
+        ruutujen_muutos_frontissa()
+        
+        # ei toimi atm D:
+        if g_over:
+            game_over()
+            break
+   
