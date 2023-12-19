79dc89897b6374e8dd1c2498b35289c270754188
Marius Ranta <marius.v.ranta@utu.fi>
2023-12-19 02:16:47 +0000

    Replace miinaharava.py

--- miinaharava.py
+++ miinaharava.py
@@ -1,12 +1,22 @@
-"""Viimeisin toimiva versio."""
+"""Miinaharava Extended Horror Experience!"""
 
 import random
 from os import system, name
 
-def kysy_koko():
-    """Pelin esittely ja pelikentän koon kysyntä.
+def tyhjenna_terminaali():
+    """Tyhjentää terminaalin käyttöjärjestelmä huomioiden."""
+    
+    # windows
+    if name == "nt":
+        _ = system("cls")
+    # mac / linux
+    else:
+        _ = system("clear")
 
-        Peli on koodattu siten, että pelikentän kokoa ja pommien lukumäärää voi halutessaan vaihtaa helposti
+def kysy_koko():
+    """Esittelee pelin ja kysyy pelikentän koon.
+
+        Peli on koodattu siten, että pelikentän kokoa ja pommien lukumäärää voi halutessaan vaihtaa helposti,
         ja koko muu peli adaptoituu niiden mukaan.
     """
     
@@ -19,29 +29,46 @@ def kysy_koko():
     print("|_____|")
     print("Pelaat peliä Miinaharava Extended Horror Experience!")
     print()
-    print("Pelilaudat: pieni 5x5, iso 10x10")
+    print("Pelilaudat: pieni \"p\" / iso \"i\"")
     print()
 
     while True:
-        koko = input("Anna pelilaudan koko: \"pieni\" / \"iso\": ")
+        koko = input("Anna koko: ")
         
-        # koko määrittää rivien/sarakkeiden määrän (n) sekä pommine lukumäärän (lkm)
-        if koko == "pieni":
+        # koko määrittää rivien/sarakkeiden määrän (n) sekä pommien lukumäärän (lkm)
+        if koko == "p":
             n = 5
             lkm = 3
             break
-        elif koko == "iso":
+        elif koko == "i":
             n = 10
             lkm = 13
             break
         else:
-            print("Opettele kirjottaa, pls!")
-            
-def aseta_pommit():
-    """Asettaa pommit satunnaisiin paikkoihin backend-matriisissa."""
+            print("Virheellinen toiminto!")
+
+def alusta_listat_ja_matriisit():
+    """Alustaa listat ja matriisit."""
     
     global mat_back
-    global lkm
+    global mat_front
+    global pommit
+    global liput
+    global nakyvat
+    
+    # backend-matriisi
+    mat_back = [[0 for rivi in range(n)] for sarake in range(n)] 
+    # frontend-matriisi
+    mat_front = [[" " for rivi in range(n)] for sarake in range(n)]
+    # pommien koordinaatit
+    pommit = []
+    # lippujen koordinaatit
+    liput = []
+    # avatut ruudut
+    nakyvat = []
+
+def aseta_pommit():
+    """Asettaa pommit satunnaisiin paikkoihin backend-matriisissa."""
     
     i = 0
     # lkm = pommien lukumäärä (määritetään kysy_koko()-funktiossa)
@@ -53,24 +80,33 @@ def aseta_pommit():
             pommit.append((sat_rivi, sat_sarake))
             i += 1           
 
+def numerointi():
+    """Numeroi backend-matriisissa kosketuksissa olevien pommien määrä per ruutu."""
+    
+    for i in range(len(mat_back)):
+        for j in range(len(mat_back[i])):
+            # jos huomataan -1 eli pommi:
+            if mat_back[i][j] == -1:
+                # rajataan alue, ja jonka kaikkien -1 poikkeavien arvojen tilalle lisätään yhtä isompi luku
+                # chatgpt setit: https://chat.openai.com/share/556805c3-c6f9-47d0-b1d3-e9bb33296c3f
+                for x in range(max(0, i-1), min(i+2, len(mat_back))):
+                    for y in range(max(0, j-1), min(j+2, len(mat_back[i]))):
+                        if mat_back[x][y] != -1:
+                            mat_back[x][y] += 1
+
 def tulosta_ruudukko() -> str:
     """Tulostaa pelilaudan ruudukkona."""
     
-    global n
-    global mat_front
-    
-    # tulostetaan taulukon katto
+    # tulostetaan ruudukon katto
     print("______" * n + "_")
     
-    # tulostetaan taulukko vaakarivi kerrallaan
+    # tulostetaan ruudukko vaakarivi kerrallaan
     for lista in mat_front:
         print("|     " * n + "|")
-        
         # iteroidaan listan alkiot, jotta saadaa rivin jokaiseen slottiin oikeat arvot
         rivi = ""
         for alkio in range(n):
             rivi += "|  " + str(lista[alkio]) + "  "
-        
         print(rivi + "|")
         print("|_____" * n + "|")
 
@@ -86,7 +122,7 @@ def pelaajan_valinta():
         try:
             # "" / "å" / "f"
             toiminto = input("Toiminto: ")
-            # jos haluu pelistä vittuun
+            # takaovi jos haluu pelistä vittuun
             if toiminto == "å":
                 exit()
             elif (toiminto != "") and (toiminto != "f"):
@@ -104,34 +140,16 @@ def pelaajan_valinta():
             print("Vain numerot kelpaavat!")
     
     # muutetaan reaalimaailman koordinaatit python-ystävällisiksi (listojen eka indeksi 0 eikä 1!)
-    x -= 1
     y -= 1
+    x -= 1  
     
-def numerointi():
-    """Numeroidaan backend-matriisissa kosketuksissa olevien pommien määrä per ruutu."""
-    
-    global mat_back
-    
-    for i in range(len(mat_back)):
-        for j in range(len(mat_back[i])):
-            # jos huomataan -1 eli pommi:
-            if mat_back[i][j] == -1:
-                # rajataan alue, ja jonka kaikkien -1 poikkeavien arvojen tilalle lisätään yhtä isompi luku
-                # chatgpt setit, linkki jossain
-                for x in range(max(0, i-1), min(i+2, len(mat_back))):
-                    for y in range(max(0, j-1), min(j+2, len(mat_back[i]))):
-                        if mat_back[x][y] != -1:
-                            mat_back[x][y] += 1
-
 def ruutujen_muutos_frontissa():
-    """Muuttaa ruutuja pelaajan antamien koordinaattien perusteella."""
+    """Muuttaa näkyviä ruutuja pelaajan antamien koordinaattien perusteella."""
     
-    global nakyvat
-    global x
-    global y
     global peli_loppu
     global voitto
-    global lkm
+    global y
+    global x
         
     # liputus
     if toiminto == "f":
@@ -157,7 +175,8 @@ def ruutujen_muutos_frontissa():
                     if mat_back[y][x] != -1:
                         mat_front[y][x] = mat_back[y][x]
                         # lisätään koordinaatit avattujen ruutujen listaan
-                        nakyvat.append((y, x))
+                        if (y, x) not in nakyvat:
+                            nakyvat.append((y, x))
         
         # jos osutaan pommiin ;)
         elif mat_back[y][x] == -1:
@@ -187,104 +206,111 @@ def ruutujen_muutos_frontissa():
     if maara == lkm:    
         peli_loppu = True
         voitto = True
-        
+
 def ohjeet_str() -> str:
-    """Tulostaa ohjeet / perusnäkymän pelin alettua."""
+    """Tulostaa ohjeet/perusnäkymän pelin aikana."""
     
     print("Miinaharava Extended Horror Experience!")
     print()
-    print("Toiminnot: ruudun avaus \"\" / liputus \"f\"")
+    print("Toiminnot: ruudun avaus \"\" / liputus \"f\"")       
 
-def tyhjenna_terminaali():
-    """Komento tyhjentää terminaalin käyttöjärjestelmän huomioiden."""
-    
-    # windows
-    if name == "nt":
-        _ = system("cls")
-    # mac / linux
-    else:
-        _ = system("clear")
-
-def tallenna_pelin_tiedot(avatut_ruudut, liputetut_ruudut):
-    """Tallentaa pelin tiedot tiedostoon."""
-    
-    with open('pelin_tiedot.txt', 'w') as tiedosto:
-        tiedosto.write(f"Avasit {avatut_ruudut} ruutua! Hyvä sinä!\n")
-        tiedosto.write(f"Liputit {liputetut_ruudut} ruutua! Mahtavaa!\n")
-    pass
-
-def tulosta_pelin_tiedot():
-    """Tulostaa pelin tiedot tiedostosta."""
-    
-    with open('pelin_tiedot.txt', 'r') as tiedosto:
-        tiedot = tiedosto.read()
-        print(tiedot)
-    pass
-
-def tappio_str() -> str:
-    print("Miinaharavasi meni rikki ja osuit neuvostoaikaiseen räjähtämättömään sirpalemiinaan.")
-    print()
-    print("Olet nyt pyörätuolipotilas lopun ikäsi. Hävisit siis pelin :D")
-    
 def voitto_str() -> str:
+    """Tulostaa ohjeet/perusnäkymän voittoa varten."""
+    
     print("Voitit pelin lol")
     print()
-    print()
+    print("Toiminnot: kyllä \"k\" / ei \"e\"")
+
+def tappio_str() -> str:
+    """Tulostaa ohjeet/perusnäkymän tappiota varten."""
     
-def alusta_listat_ja_matriisit():
-    """Alustaa listat ja matriisit."""
+    print("Miinaharavasi meni rikki ja osuit neuvostoaikaiseen räjähtämättömään sirpalemiinaan.")
+    print("Olet nyt pyörätuolipotilas lopun ikäsi. Hävisit siis pelin :D")
+    print("Toiminnot: kyllä \"k\" / ei \"e\"")
+
+def tallenna_pelin_tiedot(nakyvat, liput):
+    """Tallentaa pelin tiedot tiedostoon."""
     
-    global mat_back
-    global mat_front
-    global pommit
-    global liput
-    global nakyvat
+    with open("pelin_tiedot.txt", "w") as tiedosto:
+        tiedosto.write("____________________________________________\n")
+        tiedosto.write("Pelitiedot:\n")
+        tiedosto.write(f"Avasit {len(nakyvat)} ruutua. Hyvä sinä!\n")
+        tiedosto.write(f"Liputit {len(liput)} ruutua. Mahtavaa!\n")
+        tiedosto.write("____________________________________________")
+
+def tulosta_pelin_tiedot() -> str:
+    """Tulostaa pelin tiedot tiedostosta."""
     
-    # backend-matriisi
-    mat_back = [[0 for rivi in range(n)] for sarake in range(n)] 
-    # frontend-matriisi
-    mat_front = [[" " for rivi in range(n)] for sarake in range(n)]
-    # pommien koordinaatit
-    pommit = []
-    # lippujen koordinaatit
-    liput = []
-    # avatut ruudut
-    nakyvat = []
+    with open("pelin_tiedot.txt", "r") as tiedosto:
+        tiedot = tiedosto.read()
+        print(tiedot)
+
+def kysy_lopetus():
+    """Kysyy käyttäjältä pelitietojen tulostuksesta ja uudelleenpelaamisesta."""
+    
+    global lopeta_pelaaminen
+    
+    while True:
+        tulosta = input("Pelitietojen tulostus: ")
+        if tulosta == "k":
+            tulosta_pelin_tiedot()
+            break
+        elif tulosta == "e":
+            break
+        else:
+            print("Virheellinen toiminto!")
+    
+    while True:
+        lopetus = input("Pelin lopetus: ")
+        if lopetus == "e":
+            break
+        elif lopetus == "k":
+            lopeta_pelaaminen = True
+            break
+        else:
+            print("Virheellinen toiminto!")
+
 
 # PÄÄOHJELMA
 if __name__ == "__main__":
     """Käytännössä peli pelataan tässä."""
     
-    tyhjenna_terminaali()
+    lopeta_pelaaminen = False
     
-    # kysytään alkuarvoja
-    kysy_koko()
-    
-    # alustetaan matriisit ja listat sekä asetetaan mat_backiin pommit ja niiden ympärille numerot
-    alusta_listat_ja_matriisit()
-    aseta_pommit()
-    numerointi()
-    
-    # ruutujen_muutos_frontissa()-funktiossa testataan pommiin osumista sekä voittoa
-    peli_loppu = False
-    voitto = False
-    
-    while not peli_loppu:
+    while not lopeta_pelaaminen:
         tyhjenna_terminaali()
-        ohjeet_str()
-        tulosta_ruudukko()
-        pelaajan_valinta()
-        ruutujen_muutos_frontissa()
         
-        if peli_loppu:
-            if voitto:
-                tyhjenna_terminaali()
-                voitto_str()
-                tulosta_ruudukko()
-                exit()
-            else:
-                tyhjenna_terminaali()
-                tappio_str()
-                tulosta_ruudukko()
-                exit()
+        # kysytään alkuarvoja
+        kysy_koko()
         
+        # alustetaan matriisit ja listat sekä asetetaan mat_backiin pommit ja niiden ympärille numerot
+        alusta_listat_ja_matriisit()
+        aseta_pommit()
+        numerointi()
+        
+        # ruutujen_muutos_frontissa()-funktiossa testataan pommiin osumista sekä voittoa
+        peli_loppu = False
+        voitto = False
+        
+        while True:
+            tyhjenna_terminaali()
+            ohjeet_str()
+            tulosta_ruudukko()
+            pelaajan_valinta()
+            ruutujen_muutos_frontissa()
+            
+            if peli_loppu:
+                if voitto:
+                    tyhjenna_terminaali()
+                    voitto_str()
+                    tulosta_ruudukko()
+                    tallenna_pelin_tiedot(nakyvat, liput)
+                    break
+                else:
+                    tyhjenna_terminaali()
+                    tappio_str()
+                    tulosta_ruudukko()
+                    tallenna_pelin_tiedot(nakyvat, liput)
+                    break
+        kysy_lopetus()
+    exit()
